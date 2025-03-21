1f858b03f47c098783f28adaf0cf230100bd8992
spacebear <git@spacebear.dev>
2025-03-21 14:14:35 -0400

    Add SerdeJsonError mapping
    Handle error conversions for `to_json` and `from_json` helpers.

--- src/error.rs
+++ src/error.rs
@@ -199,3 +199,15 @@ impl From<EncapsulationError> for PayjoinError {
         PayjoinError::EncapsulationError { message: format!("{:?}", value) }
     }
 }
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[error("Error de/serializing JSON object: {message}")]
+#[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+pub struct SerdeJsonError {
+    message: String,
+}
+impl From<serde_json::Error> for SerdeJsonError {
+    fn from(value: serde_json::Error) -> Self {
+        SerdeJsonError { message: format!("{:?}", value) }
+    }
+}
