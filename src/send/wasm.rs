3a1b489473b866e0b369228747d4082fea9d7d51
River Kanies <river.kanies@gmail.com>
2025-03-26 13:10:56 -0500

    WIP: send post and get contexts

--- src/send/wasm.rs
+++ src/send/wasm.rs
@@ -1,15 +1,6 @@
-use std::str::FromStr;
-use std::time::Duration;
+use crate::ohttp::ClientResponse;
 
-// use payjoin::bitcoin::psbt::Psbt;
-// use payjoin::bitcoin::FeeRate;
-// use payjoin::receive as pdk;
-
-use crate::bitcoin_ffi::Network;
-// use crate::error::PayjoinError;
-use crate::ohttp::OhttpKeys;
 use crate::Url;
-use crate::error::PayjoinError;
 
 use {
   crate::utils::result::JsResult,
@@ -74,11 +65,93 @@ impl From<super::Sender> for Sender {
 #[wasm_bindgen]
 impl Sender {
     /// Extract serialized Request and Context from a Payjoin Proposal.
-    pub fn extract_v2(&self, ohttp_relay: String) -> JsResult<Request> {//(Request, V2PostContext)> {
-      let url = Url::parse(ohttp_relay)?;
+    pub fn extract_v2(&self, ohttp_relay: String) -> JsResult<RequestV2PostContext> {
+        let url = Url::parse(ohttp_relay)?;
         match self.0.extract_v2(url.into()) {
-            Ok((req, _ctx)) => Ok(req.into()),//(req.into(), ctx.into())),
+            Ok((req, ctx)) => Ok(RequestV2PostContext::new(req.into(), ctx.into())),
             Err(e) => Err(e.into()),
         }
+    }
+}
+
+#[wasm_bindgen]
+pub struct V2PostContext(payjoin::send::v2::V2PostContext);
+
+impl From<payjoin::send::v2::V2PostContext> for V2PostContext {
+    fn from(ctx: payjoin::send::v2::V2PostContext) -> Self {
+        Self(ctx)
+    }
+}
+
+#[wasm_bindgen]
+impl V2PostContext {
+    // consumes self, so V2PostContext won't be available in js after calling this
+    pub fn process_response(self, response: &[u8]) -> JsResult<V2GetContext> {
+        self.0.process_response(response)
+            .map(Into::into)
+            .map_err(Into::into)
+    }
+}
+
+#[wasm_bindgen]
+pub struct V2GetContext(payjoin::send::v2::V2GetContext);
+
+impl From<payjoin::send::v2::V2GetContext> for V2GetContext {
+    fn from(ctx: payjoin::send::v2::V2GetContext) -> Self {
+        Self(ctx)
+    }
+}
+
+#[wasm_bindgen]
+impl V2GetContext {
+    pub fn extract_req(&self, ohttp_relay: String) -> JsResult<RequestOhttpContext> {
+        self.0.extract_req(payjoin::Url::parse(&ohttp_relay)?)
+            .map(|(request, ctx)| RequestOhttpContext::new(request.into(), ctx.into()))
+            .map_err(Into::into)
+    }
+
+    pub fn process_response(&self, response: &[u8], ohttp_ctx: &ClientResponse) -> JsResult<Option<String>> {
+        self.0.process_response(response, ohttp_ctx.into())
+            .map(|opt_psbt| opt_psbt.map(|psbt| psbt.to_string()))
+            .map_err(Into::into)
+    }
+}
+
+#[wasm_bindgen]
+pub struct RequestOhttpContext (Request, ClientResponse);
+
+impl RequestOhttpContext {
+    pub fn new(request: Request, ohttp_ctx: ClientResponse) -> Self {
+        Self(request, ohttp_ctx)
+    }
+
+    pub fn request(&self) -> Request {
+        self.0.clone()
+    }
+
+    pub fn ohttp_ctx(self) -> ClientResponse {
+        self.1
+    }
+}
+
+#[wasm_bindgen]
+pub struct RequestV2PostContext (Request, V2PostContext);
+
+#[wasm_bindgen]
+impl RequestV2PostContext {
+    #[wasm_bindgen(constructor)]
+    pub fn new(request: Request, context: V2PostContext) -> Self {
+        Self(request, context)
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn request(&self) -> Request {
+        self.0.clone() // Assuming Request implements Clone
+    }
+
+    // consumes self, so RequestV2PostContext won't be available in js after getting context, however, using destructuring on the js end makes this seemless.
+    #[wasm_bindgen(getter)]
+    pub fn context(self) -> V2PostContext {
+        self.1
     }
 }
