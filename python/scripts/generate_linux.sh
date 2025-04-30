43adf79fb8bbb161d61ec8351a5b490331d647b0
DanGould <d@ngould.dev>
2025-04-30 19:28:15 -0400

    Remove redundancy in generate_linux.sh
    Follow the lead of the prior generate_macos scripts.

--- python/scripts/generate_linux.sh
+++ python/scripts/generate_linux.sh
@@ -5,18 +5,18 @@ ${PYBIN}/pip install -r requirements.txt -r requirements-dev.txt
 LIBNAME=libpayjoin_ffi.so
 LINUX_TARGET=x86_64-unknown-linux-gnu
 
-echo "Generating payjoin_ffi.py..."
 cd ../
-# This is a test script the actual release should not include the test utils feature
-cargo build --profile release --features uniffi,_test-utils
-cargo run --profile release --features uniffi,_test-utils --bin uniffi-bindgen generate --library target/release/$LIBNAME --language python --out-dir python/src/payjoin/
+
+rustup target add $LINUX_TARGET
 
 echo "Generating native binaries..."
-rustup target add $LINUX_TARGET
 # This is a test script the actual release should not include the test utils feature
-cargo build  --profile release-smaller --target $LINUX_TARGET --features uniffi,_test-utils
+cargo build --profile release-smaller --target $LINUX_TARGET --features uniffi,_test-utils
+
+echo "Generating payjoin_ffi.py..."
+cargo run --profile release --features uniffi,_test-utils --bin uniffi-bindgen generate --library target/$LINUX_TARGET/release-smaller/$LIBNAME --language python --out-dir python/src/payjoin/
 
 echo "Copying linux payjoin_ffi.so"
-cp target/$LINUX_TARGET/release-smaller/$LIBNAME python/src/payjoin/$LIBNAME
+cp target/release-smaller/$LIBNAME python/src/payjoin/$LIBNAME
 
 echo "All done!"
